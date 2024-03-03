cb5b75a8e9384f948ad33a630de75fbfc9374697
MomorioUHT <spirezaza.janthai000@gmail.com>
2024-03-03 17:15:57 +0700

    Update2

--- PrimeFromXtoY.py
+++ PrimeFromXtoY.py
@@ -1,10 +1,15 @@
-import math
-def isPrime(x: int):
+import math 
+def isPrime(x: int): #Smart
     if (x == 2): return 1
     if (x == 1 or x % 2 == 0): return 0
     for i in range(3, math.ceil(math.sqrt(x))+1, 2):
         if (x % i == 0): return 0
     return 1
+
+def prime2(x: int): #Idiot
+  for i in range(3,x):
+    if x % i == 0: return 0
+  return 1
 
 ls = [int(i) for i in str(input()).split(" to ")]
 for i in range(ls[0],ls[1]+1):
