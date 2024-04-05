87b7c89f30a011d9eb6b4df1d6321771bcbd6ef4
MomorioUHT <spirezaza.janthai000@gmail.com>
2024-04-05 12:15:15 +0700

    A

---
+++ ManualBarChart2.py
@@ -0,0 +1,14 @@
+digits = [int(n) for n in str(input()).split("-")]
+rows = max(digits)
+bar_graph = []
+for row in range(rows, 0, -1):
+    bar_graph.append(['U' if digit >= row else ' ' for digit in digits])
+    
+i = rows
+for row in bar_graph:
+    if (len(str(rows)) == 2): print(f"{i:2d}|", end=" ")
+    else: print(f"{i}|", end=" ")
+    print(' '.join(row))
+    i-=1
+    
+print(f"{'-'*len(str(rows))}-{'-'*((len(digits)*2))}")
