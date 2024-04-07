4dcf4ad5401665c1a67fcd53655d807818a9d391
MomorioUHT <spirezaza.janthai000@gmail.com>
2024-04-07 21:46:17 +0700

    FIX

--- 2DArray.py
+++ 2DArray.py
@@ -11,7 +11,13 @@ for i in range(dim[1]):
 for i in range(dim[1]):
     for j in range(dim[0]):
         number = int(board[i][j])
-        if num >=100: print(f"{number:3d} ", end="")
-        elif num >= 10: print(f"{number:2d} ", end="")
-        else: print(f"{number} ", end="")
+        if num >=100: 
+            if j == dim[0]-1: print(f"{number:3d}", end="")
+            else: print(f"{number:3d} ", end="")
+        elif num >= 10: 
+            if j == dim[0]-1: print(f"{number:2d}", end="")
+            else: print(f"{number:2d} ", end="")
+        else: 
+            if j == dim[0]-1: print(f"{number}", end="")
+            else: print(f"{number} ", end="")
     print("")
