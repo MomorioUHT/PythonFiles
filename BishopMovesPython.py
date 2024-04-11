369b58f33af01efdb1cdba93f044f52172649ab9
MomorioUHT <spirezaza.janthai000@gmail.com>
2024-04-11 17:51:16 +0700

    6

--- BishopMovesPython.py
+++ BishopMovesPython.py
@@ -17,18 +17,17 @@ def bishop_moves(board, x, y):
                 board[i][j] = ' '
 
     board[y][x] = 'B'
+    
+    
+board = [[' ' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+pos_x, pos_y = map(int, input().split())
 
-if __name__ == "__main__":
-    board = [[' ' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+bishop_moves(board, pos_x, pos_y)
 
-    pos_x, pos_y = map(int, input().split())
-
-    bishop_moves(board, pos_x, pos_y)
-
-    print("  0 1 2 3 4 5 6 7")
-    print("------------------")
-    for i in range(BOARD_SIZE):
-        print(f"{i}|", end="")
-        for j in range(BOARD_SIZE):
-            print(f"{board[i][j]}|", end="")
-        print("\n------------------")
+print("  0 1 2 3 4 5 6 7")
+print("------------------")
+for i in range(BOARD_SIZE):
+    print(f"{i}|", end="")
+    for j in range(BOARD_SIZE):
+        print(f"{board[i][j]}|", end="")
+    print("\n------------------")
