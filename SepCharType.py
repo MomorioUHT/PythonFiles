0d6a0f680fa489b936614dcbc9caccbb50612616
MomorioUHT <spirezaza.janthai000@gmail.com>
2024-05-17 18:35:23 +0700

    aeghe

---
+++ SepCharType.py
@@ -0,0 +1,13 @@
+context = str(input())
+vowels = ' '.join([i for i in context if i.lower() in ["a","e","i","o","u"]])
+alpha = ' '.join([i for i in context if i.lower() not in ["a","e","i","o","u"] and i.isalpha()])
+number = ' '.join([i for i in context if i.isdigit()])
+if vowels != "": 
+    print("Vowel(s):")
+    print(vowels)
+if alpha != "": 
+    print("Alphabet(s):")
+    print(alpha)
+if number != "": 
+    print("number(s):")
+    print(number)
